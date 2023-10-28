3d0742c995198a8e4a4b45a4aeeea711527772cd
Martin Strömberg <contact@martinstromberg.se>
2023-10-28 13:16:13 +0200

    Add POST, PUT, and DELETE verbs
    This commit should mostly finish the project, but I'm not particularly
    pleased as I don't think I fully understand what I was doing and why I
    was doing it.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Rust+HTMX Todo
 
 A simple Todo web app written in Rust (using Rocket) and utilizing HTMX
+
+I consider this little exercise a failure as I don't think I fully understand
+what I'm doing and why I'm doing it. I'll have to return to this repo at some 
+point in the future if I decide to give Rust a proper go.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use rocket::http::Status;
 use rocket::{response::Redirect, State};
+use rocket::form::Form;
 use rocket_dyn_templates::{Template, context};
 use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
 
 #[macro_use] extern crate rocket;
 
@@ -11,7 +14,17 @@ pub struct Todo {
     pub completed: bool,
 }
 
-pub type TodoList = Vec<Todo>;
+pub type TodoList = Mutex<Vec<Todo>>;
+
+#[derive(FromForm)]
+struct PostData<'r> {
+    r#description: &'r str,
+}
+
+#[derive(FromForm)]
+struct PutData {
+    completed: bool,
+}
 
 #[get("/")]
 fn site_index() -> Redirect {
@@ -20,15 +33,19 @@ fn site_index() -> Redirect {
 
 #[get("/")]
 fn get_todos_page(items: &State<TodoList>) -> Template {
+    let lock = items.lock().expect("lock app state");
+
     Template::render("index", context! {
         title: "Todos",
-        items: items.to_vec(),
+        items: lock.to_vec(),
     })
 }
 
 #[get("/<id>")]
 fn get_todo_item(id: i32, items: &State<TodoList>) -> Option<Template> {
-    items
+    let lock = items.lock().expect("lock app state");
+
+    lock
         .iter()
         .find(|&ti| ti.id == id)
         .map(|ti| {
@@ -40,13 +57,62 @@ fn get_todo_item(id: i32, items: &State<TodoList>) -> Option<Template> {
         })
 }
 
+#[post("/", data = "<data>")]
+fn post_todo(data: Form<PostData<'_>>, items: &State<TodoList>) -> Template {
+    let mut lock = items.lock().expect("lock app state");
+
+    let id: i32 = 1 + lock
+        .iter()
+        .fold(0, |acc, item| if item.id > acc { item.id } else { acc });
+    let desc = data.description.to_string();
+    lock.push(Todo { id, description: desc.clone(), completed: false });
+
+    Template::render("todo-item", context! {
+        id: id,
+        description: desc.clone(),
+        completed: false,
+    })
+}
+
+#[put("/<id>", data = "<data>")]
+fn put_todo(id: i32, data: Form<PutData>, items: &State<TodoList>) -> Option<Template> {
+    let mut lock = items.lock().expect("lock app state");
+
+    lock
+        .iter_mut()
+        .find(|t| t.id == id)
+        .map(|t| {
+            t.completed = data.completed;
+
+            Template::render("todo-item", context! {
+                id: t.id,
+                description: t.description.clone(),
+                completed: t.completed,
+            })
+        })
+}
+
+#[delete("/<id>")]
+fn delete_todo(id: i32, items: &State<TodoList>) -> Status {
+    let mut lock = items.lock().expect("lock app state");
+    lock
+        .iter()
+        .position(|t| t.id == id)
+        .map(|i| {
+            _ = lock.remove(i);
+
+            Status::Ok
+        })
+        .unwrap_or(Status::NotFound)
+}
+
 #[launch]
 fn rocket() -> _ {
-    let items: TodoList = vec![Todo {
+    let items: TodoList = Mutex::new(vec![Todo {
         id: 1,
         description: "Finish the demo".to_string(),
         completed: false,
-    }];
+    }]);
 
     rocket::build()
         .manage(items)
@@ -54,6 +120,9 @@ fn rocket() -> _ {
         .mount("/todos", routes![
             get_todos_page,
             get_todo_item,
+            post_todo,
+            put_todo,
+            delete_todo,
         ])
         .attach(Template::fairing())
 }
