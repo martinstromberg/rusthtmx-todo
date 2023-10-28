ca8d4790d9ab7dfaaf5b77e72db0470a15a8121b
Martin Strömberg <contact@martinstromberg.se>
2023-10-28 11:43:00 +0200

    Add index and item rendering
    I don't like this language - please send help.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-use rocket::response::Redirect;
+use rocket::{response::Redirect, State};
 use rocket_dyn_templates::{Template, context};
+use serde::{Deserialize, Serialize};
 
 #[macro_use] extern crate rocket;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Todo {
+    pub id: i32,
+    pub description: String,
+    pub completed: bool,
+}
+
+pub type TodoList = Vec<Todo>;
 
 #[get("/")]
 fn site_index() -> Redirect {
@@ -9,17 +19,42 @@ fn site_index() -> Redirect {
 }
 
 #[get("/")]
-fn get_todos_page() -> Template {
+fn get_todos_page(items: &State<TodoList>) -> Template {
     Template::render("index", context! {
-        title: "Todo App",
+        title: "Todos",
+        items: items.to_vec(),
     })
+}
+
+#[get("/<id>")]
+fn get_todo_item(id: i32, items: &State<TodoList>) -> Option<Template> {
+    items
+        .iter()
+        .find(|&ti| ti.id == id)
+        .map(|ti| {
+            Template::render("todo-item", context! {
+                id: ti.id,
+                description: ti.description.clone(),
+                completed: ti.completed,
+            })
+        })
 }
 
 #[launch]
 fn rocket() -> _ {
+    let items: TodoList = vec![Todo {
+        id: 1,
+        description: "Finish the demo".to_string(),
+        completed: false,
+    }];
+
     rocket::build()
+        .manage(items)
         .mount("/", routes![site_index])
-        .mount("/todos", routes![get_todos_page])
+        .mount("/todos", routes![
+            get_todos_page,
+            get_todo_item,
+        ])
         .attach(Template::fairing())
 }
 
